1f0bfe4274786c0e6a49d29d06fea8d914c02d69
sabrinazhsa <sabrinanisa58@gmail.com>
2026-09-04 14:27:08 +0800

    tes

---
+++ src/main/java/com/mycompany/jasamakeupartis/JasaMakeUpArtis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jasamakeupartis;
+
+/**
+ *
+ * @author HP VICTUS
+ */
+public class JasaMakeUpArtis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
